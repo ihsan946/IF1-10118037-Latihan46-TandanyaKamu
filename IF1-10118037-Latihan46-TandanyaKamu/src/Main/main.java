4e6a1565645661b50fe9b1479942efcf11c6d513
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-10-20 10:33:20 +0700

    Add files via upload

---
+++ IF1-10118037-Latihan46-TandanyaKamu/src/Main/main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+
+/**
+ *
+ * @author Muhammad Ihsan
+ * NAMA       : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM          : 10118037
+ * Deskripsi Program   : Program ini berisi program tandanya kamu berdasar
+ *                                        tahun lahir
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Age age = new Age(2019);
+        
+    }
+    
+}
